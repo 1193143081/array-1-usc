2d8bca02349c1a43083900f4d83b0da1780126ed
Juan David Gil Ramirez <114194761+1193143081@users.noreply.github.com>
2022-10-21 16:38:24 -0500

    Add files via upload
    parcial2

---
+++ pApo2II/Entregable.java
@@ -0,0 +1,12 @@
+package pApo2;
+
+public interface Entregable {
+
+	void entregar();
+
+	void devolver();
+
+	boolean isEntregado();
+	int compareTo(Object a);
+
+}
---
+++ pApo2II/Interface.java
@@ -0,0 +1,12 @@
+package pApo2;
+
+public interface Interface {
+
+	 public void entregar();
+
+	 public void devolver();
+
+	 public boolean isEntregado();
+
+	 public int compareTo(Object a);
+}
